ba27e380e67e7ddb2f19f48033075a3a5dbf35b0
Sverre Nystad <sverrnys@stud.ntnu.no>
2023-07-08 00:35:55 +0200

    Feat: create a main menu

--- Assets/Scripts/GameStates/GameSceneManager.cs
+++ Assets/Scripts/GameStates/GameSceneManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void LoadIntoMainMenu()
     {
-        
+        SceneManager.LoadScene("MainMenu");
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void LoadIntoGame()
     {
-        
+        SceneManager.LoadScene("GameMap");
+    }
+
+    public static void LoadIntoScoreBoard()
+    {
+        SceneManager.LoadScene("ScoreBoard");
     }
 }
