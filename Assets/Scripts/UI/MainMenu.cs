c1498b059181d733015126f01d978200649821f4
Elin Haugum <elin.haugum@hotmail.com>
2023-07-09 18:32:09 +0200

    Add tutorial

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,15 +8,35 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button playBtn;
     [SerializeField] private Button quitBtn;
+    [SerializeField] private Button soundsGoodBtn;
+    public GameObject[] popups;
 
-    void Awake()
+    void Start()
     {
         playBtn.onClick.AddListener(() => {
-            GameSceneManager.LoadIntoGame();
+            popup();
         });
         
         quitBtn.onClick.AddListener(() => {
             Application.Quit();
         });
+
+        soundsGoodBtn.onClick.AddListener(() => {
+            GameSceneManager.LoadIntoGame();
+        });
+
+        popups = GameObject.FindGameObjectsWithTag("Popup");
+        foreach (var popup in popups)
+        {
+            popup.SetActive(false);
+        }
+    }
+
+    void popup()
+    {
+        foreach (var popup in popups)
+        {
+            popup.SetActive(true);
+        }
     }
 }
